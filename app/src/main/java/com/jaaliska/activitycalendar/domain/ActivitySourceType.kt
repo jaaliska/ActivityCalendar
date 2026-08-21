0c1720d8696c364b76cf34088c5278201e9be9d2
jaaliskа <6608424+jaaliska@users.noreply.github.com>
2026-08-21 19:33:34 +0200

    Add the Activity domain model

---
+++ app/src/main/java/com/jaaliska/activitycalendar/domain/ActivitySourceType.kt
@@ -0,0 +1,8 @@
+package com.jaaliska.activitycalendar.domain
+
+/** Where a stored activity came from. */
+enum class ActivitySourceType {
+    HEALTH_CONNECT,
+    GARMIN_CSV,
+    MANUAL,
+}
---
+++ app/src/main/java/com/jaaliska/activitycalendar/domain/ActivityType.kt
@@ -0,0 +1,12 @@
+package com.jaaliska.activitycalendar.domain
+
+/** Activity types of the app; types coming from a source are mapped onto these. */
+enum class ActivityType {
+    BADMINTON,
+    CYCLING,
+    RUNNING,
+    STRENGTH_TRAINING,
+    WALKING,
+    YOGA,
+    UNKNOWN,
+}
